f93c1e6a5b7840b4297d9e0678876f045cf6136e
ilhamsurya <ilhamsurya.m@gmail.com>
2021-05-02 16:14:17 +0700

    landing page finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,6 +90,8 @@ module.exports = {
       'gradient-to-bl': 'linear-gradient(to bottom left, var(--tw-gradient-stops))',
       'gradient-to-l': 'linear-gradient(to left, var(--tw-gradient-stops))',
       'gradient-to-tl': 'linear-gradient(to top left, var(--tw-gradient-stops))',
+      'hero-pattern': "url('/img/hero-pattern.svg')",
+      'footer-texture': "url('/app/static/assets/img/data analysis.png')",
     },
     backgroundOpacity: (theme) => theme('opacity'),
     backgroundPosition: {
