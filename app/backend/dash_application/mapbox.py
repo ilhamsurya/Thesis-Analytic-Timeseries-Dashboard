4eac2545539aa7cb254fe294ce9898ff478997fa
ilhamsurya <ilhamsurya.m@gmail.com>
2021-06-03 12:48:38 +0700

    fix frontend

--- app/backend/dash_application/mapbox.py
+++ app/backend/dash_application/mapbox.py
@@ -7,29 +7,3 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Load DataFrame
-df = create_dataframe()
-df["text"] = df["kategori"] + ", " + df["tempat_kejadian"]
-
-fig = go.Figure(
-    data=go.Scattergeo(
-        lon=df["latitude"],
-        lat=df["longitude"],
-        text=df["text"],
-        mode="markers",
-    )
-)
-
-fig.update_layout(geo_scope="usa")
-
-# Create Layout
-mapbox = html.Div(
-    children=[
-        html.H1(children="Identified Geothermal Systems of the Western USA"),
-        html.Div(
-            children="""
-        This data was provided by the USGS.
-    """
-        ),
-        dcc.Graph(id="example-map", figure=fig),
-    ]
-)
