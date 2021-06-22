a0b8dcce69f8234339d3cc1d0ad6a548bff76783
ilhamsurya <ilhamsurya.m@gmail.com>
2021-06-22 12:05:10 +0700

    add wsgi

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from app import app
+from app import dash_app1
+
+application = DispatcherMiddleware(
+    app,
+    {
+        "/timeseries": dash_app1.app,
+    },
+)
+
+
+run_simple("0.0.0.0", 8080, app, use_reloader=True, use_debugger=True)
